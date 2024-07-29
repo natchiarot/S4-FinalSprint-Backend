814165a10fa074834e6643ee1a51d15e2d97ab89
Nat <142550205+natchiarot@users.noreply.github.com>
2024-07-29 12:32:35 -0230

    Add files via upload

---
+++ s4sprint/src/main/java/com/sprint/s4sprint/S4sprintApplication.java
@@ -0,0 +1,13 @@
+package com.sprint.s4sprint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S4sprintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S4sprintApplication.class, args);
+	}
+
+}
---
+++ s4sprint/src/test/java/com/sprint/s4sprint/S4sprintApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sprint.s4sprint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S4sprintApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
